4923d448d6ae90165aaf70330c7309bb72a95142
severinum <sewerynmichota@gmail.com>
2022-05-09 21:57:45 +0100

    Tree travelsal - preorder, inorder, postorder

---
+++ Trees/03_Tree_traverlas.py
@@ -0,0 +1,70 @@
+
+class BinaryTree(object):
+
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+    def insertLeft(self, newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRightChild(self):
+        return self.rightChild
+    
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+
+    # Tree traversal
+    def preorder(self):
+        print(self.key)
+        if self.leftChild:
+            self.leftChild.preorder()
+        if self.rightChild:
+            self.rightChild.preorder()
+
+    def inorder(self):
+        if self.leftChild:
+            self.leftChild.inorder()
+        print(self.key)
+        if self.rightChild:
+            self.rightChild.inorder()
+
+    def postorder(self):
+        if self.leftChild:
+            self.leftChild.postorder()
+        if self.rightChild:
+            self.rightChild.postorder()
+        print(self.key)
+
+#=========  TESTING =============
+
+r = BinaryTree('a')
+r.insertLeft('b')
+r.insertRight('c')
+r.insertLeft('d')
+print("\nPreorder traversal:")
+r.preorder()
+print("\nInorder traversal:")
+r.inorder()
+print("\nPostorder traversal:")
+r.postorder()
